a54dd1e961f77c9f53cbdfce6cd2f556ee20574d
Gabriel Cézar <gabrielcezar326133@gmail.com>
2026-02-20 11:22:25 -0300

    Adicionado admin visual

--- motel/admin.py
+++ motel/admin.py
@@ -1,3 +1,45 @@
 from django.contrib import admin
+from .models import Cliente, Quarto, Reserva, Produto, Pagamento, Funcionario, Consumo
 
-# Register your models here.
+
+@admin.register(Cliente)
+class ClienteAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'cpf', 'email', 'data_cadastro')
+    search_fields = ('nome', 'cpf')
+
+
+@admin.register(Quarto)
+class QuartoAdmin(admin.ModelAdmin):
+    list_display=('numero', 'tipo','valor')
+    search_fields=('numero', 'tipo')
+
+
+@admin.register(Reserva)
+class ReservaAdmin(admin.ModelAdmin):
+    list_display=('quarto','cliente','data_entrada','data_saida','pago')
+    search_fields=('cliente__nome','quarto__numero')
+    list_filter=('pago',)
+
+@admin.register(Produto)
+class ProdutoAdmin(admin.ModelAdmin):
+    list_display=('nome', 'valor')
+    search_fields=('nome', 'valor')
+
+@admin.register(Pagamento)
+class PagamentoAdmin(admin.ModelAdmin):
+    list_display=('reserva', 'tipo')
+    search_fields=('reserva__id', 'tipo')
+
+@admin.register(Funcionario)
+class FuncionarioAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'cpf', 'email', 'tipo')
+    search_fields = ('nome', 'cpf', 'tipo')
+
+@admin.register(Consumo)
+class ConsumoAdmin(admin.ModelAdmin):
+    list_display = ('produto', 'quarto_numero', 'quantidade',  'reserva')
+    search_fields = ('produto__nome', 'reserva__quarto__numero', 'reserva__quarto__tipo')
+    def quarto_numero(self, obj):
+        return obj.reserva.quarto.numero
+    quarto_numero.short_description = 'Quarto'
+
--- motel/models.py
+++ motel/models.py
@@ -5,19 +5,16 @@ class Cliente(models.Model):
     cpf = models.CharField(max_length=11, unique=True)
     email = models.EmailField()
     data_cadastro = models.DateField()
+    def __str__(self):
+        return f"{self.nome} ({self.cpf})"
+    
+TIPO_QUARTO =[
+    ('BAS', 'BÁSICO'),
+    ('VIP', 'PREMIUM')
+]
 
-class Quarto(models.Model):
-    numero = models.IntegerField()
-    valor = models.DecimalField(max_digits=8, decimal_places=2)
-    tipo = models.CharField(max_length=10)
-    ocupado = models.BooleanField(default=False)
 
-class Reserva(models.Model):
-    cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE)
-    quarto = models.ForeignKey(Quarto, on_delete=models.PROTECT)
-    data_entrada = models.DateTimeField()
-    data_saida = models.DateTimeField()
-    pago = models.BooleanField(default=False)
+
 
 class Produto(models.Model):
     nome = models.CharField(max_length=100)
@@ -29,9 +26,6 @@ TIPO_PAGAMENTO = [
     ('DIN', 'Dinheiro à vista'),
     ('PIX', 'PIX'),
 ]
-class Pagamento(models.Model):
-    reserva = models.ForeignKey(Reserva, on_delete=models.CASCADE)
-    tipo = models.CharField(max_length=20, choices=TIPO_PAGAMENTO)
 
 
 TIPO_FUNCIONARIO = [
@@ -44,3 +38,35 @@ class Funcionario(models.Model):
     email = models.EmailField()
     cpf = models.CharField(max_length=11, unique=True)
     tipo = models.CharField(max_length=3, choices=TIPO_FUNCIONARIO)
+
+class Consumo(models.Model):
+    produto = models.ForeignKey(Produto, on_delete=models.PROTECT)
+    reserva = models.ForeignKey('Reserva', on_delete=models.PROTECT)
+    quantidade = models.PositiveIntegerField(default=1)
+    def __str__(self):
+        return f"Consumo: #{self.produto} - {self.reserva}"
+    
+class Quarto(models.Model):
+    numero = models.IntegerField()
+    valor = models.DecimalField(max_digits=8, decimal_places=2)
+    tipo = models.CharField(max_length=10, choices=TIPO_QUARTO)
+    ocupado = models.BooleanField(default=False)
+    def __str__(self):
+        return f"Quarto {self.numero} - {self.get_tipo_display()}"
+
+
+    
+class Pagamento(models.Model):
+    reserva = models.ForeignKey('Reserva', on_delete=models.CASCADE)
+    tipo = models.CharField(max_length=20, choices=TIPO_PAGAMENTO)
+    def __str__(self):
+        return f"Pagamento: #{self.reserva} - {self.tipo}"
+
+class Reserva(models.Model):
+    cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE)
+    quarto = models.ForeignKey(Quarto, on_delete=models.PROTECT)
+    data_entrada = models.DateTimeField()
+    data_saida = models.DateTimeField()
+    pago = models.BooleanField(default=False)
+    def __str__(self):
+        return f"Reserva #{self.id}- {self.cliente} - {self.quarto}"
